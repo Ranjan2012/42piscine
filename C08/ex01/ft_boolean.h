9843d58a31151963341217dc0dd13e0e141c2ce0
Ranjan2012 <96380657+Ranjan2012@users.noreply.github.com>
2021-12-20 00:35:53 +1030

    Add files via upload

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmallawa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 12:06:37 by rmallawa          #+#    #+#             */
+/*   Updated: 2021/12/14 16:30:59 by rmallawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define EVEN(nbr) (!(nbr&1))
+
+typedef int	t_bool;
+
+#endif
